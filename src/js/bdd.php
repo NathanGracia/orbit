319c885427798cc5ffa60d525d82852e55024bc2
DeusKiwi <nathan.gracia.863@gmail.com>
2019-12-10 01:39:50 +0100

    safe request bdd

--- src/js/bdd.php
+++ src/js/bdd.php
@@ -3,9 +3,10 @@ session_start();
 if(!empty($_SESSION['name'])){
     $bdd = new PDO('mysql:host=remotemysql.com;dbname=lespxweO1Y;charset=utf8', 'lespxweO1Y', 'EAtzAiItZE');
 
+    $safe_name = mysql_real_escape_string($_SESSION['name']);
     $reponse = $bdd->query("
 INSERT INTO score (score, name)
-VALUES (".$_POST['score'].", ' ".$_SESSION['name']."')
+VALUES (".$_POST['score'].", ' ". $safe_name."')
 ON DUPLICATE KEY UPDATE
 score = (CASE 
               WHEN values(score) > score THEN VALUES(score)
